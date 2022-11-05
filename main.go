60fe363609fafff02491fa663bc288e81d26b9a4
fahrettinbyrm <git.fahrettin@gmail.com>
2022-11-05 22:28:54 +0300

    Üsal testi yazıldı

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-turk/konya/database"
+	"github.com/go-turk/konya/urun"
+)
+
+func main() {
+	db, err := database.Connection()
+	if err != nil {
+		fmt.Println("Database Connection Hatası:", err)
+		return
+	}
+	urunRepo, err := urun.NewUrunRepository(db)
+	if err != nil {
+		fmt.Println("Urun Repo Hatası:", err)
+		return
+	}
+	backendLv2Rozeti := urun.NewUrun("Backend Lv2 Rozeti", "Siyah", 25.15)
+	if err := urunRepo.Create(backendLv2Rozeti); err != nil {
+		fmt.Println("Backend Lv2 Rozeti Eklenemedi", err)
+		return
+	}
+}
